a16a27ceff6d2620c2810dc2bda4291c4581b4a6
Guilherme Totte <gtottecoutinho@gmail.com>
2025-06-01 23:09:03 -0300

    adição da validação da input do item para que o usuário não adicione um item sem nome

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
-    
+
+    if (itemName === "") {
+        alert("Não é possível adicionar um item em branco!")
+        return
+    }
+
     const item = {
         name: itemName,
         checked: false
